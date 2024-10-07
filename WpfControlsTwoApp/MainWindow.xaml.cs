756a72e60cefd6e52288a89aa1e3df8f29f5d7e2
Maxim Efimov <efimov.top.academy@gmail.com>
2024-10-07 19:51:10 +0300

    Добавьте файлы проекта.

---
+++ WpfControlsTwoApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsTwoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Expander_Expanded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Expander_Collapsed(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
